4c6e315433beac0a028cc0b7af9650285b9d0b1a
denisa lupu <denisalupu2006@gmail.com>
2025-09-17 18:34:52 +0300

    Am facut tema7

---
+++ sprint7-js-intro/ex2.js
@@ -0,0 +1,18 @@
+//Scrieți o funcție numită tellFortune care:
+//primește 4 argumente: numărul de copii (N), numele partenerului (Z), locația geografică (Y), locul de muncă (X). afișează pe ecranul dvs. în felul următor: "Vei fi un X în Y, căsătorie cu Z și vei avea N copii." 
+// În rezolvarea problemei, vom încerca să abordăm șirurile templetizate (${variabila} este o variabilă.).
+//Exemplul 1:
+
+//Input: numarCopii: 3, numePartener: “Emanuel”, locatieGeografica: “Italia”, locMunca: “Programator”
+//Output: „Vei fi un Programator în Italia , căsătorie cu Emanuel și vei avea 3 copii.
+
+function tellFortune(numarCopii, numePartener, locatieGeografica, locMunca) {
+    return "Vei fi un " + (locMunca) + " in " + (locatieGeografica) + " casatorit cu " + (numePartener) + " si vei avea " + (numarCopii) + " copii ";
+}
+    
+
+const text1 = tellFortune(3, "Emanuel", "Italia", "Programator");
+console.log(text1) 
+
+const text2 = tellFortune(2,"Denisa", "Madrid", "Bucatar");
+console.log(text2)
---
+++ sprint7-js-intro/ex3.js
@@ -0,0 +1,42 @@
+//Folosind instrucțiunea JavaScript switch se va obține ziua săptămânii
+//Veți crea o funcție care va returna un șir de caractere (string) care va reprezenta numele unei zile a săptămânii în raport cu numărul acesteia pe parcursul unei săptămâni.
+
+//Exemplu 1:
+//Input: 1
+//Output: “Luni”
+
+//Exemplu 2:
+//Input: 5
+//Output: “Vineri”
+
+function getDay (numar) {
+    switch (numar) {
+        case "0":
+        day = "Sunday";
+        break;
+        case 1:
+        day = "Monday";
+        break;
+        case 2:
+        day = "Tuesday";
+        break;
+        case 3:
+        day = "Wednesday";
+        break;
+        case 4:
+        day = "Thursday";
+        break;
+        case 5:
+        day = "Friday";
+        break;
+        case 6:
+        day = "Saturday";
+        break;
+        default:
+            return"Error";
+    }
+}
+
+console.log (getDay(0))
+console.log (getDay(4))
+console.log (getDay(7))
