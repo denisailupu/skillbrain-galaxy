64cf09c8d154be5ca1a95d82ec696c7d798844ae
denisa lupu <denisalupu2006@gmail.com>
2025-09-30 21:51:43 +0300

    Am inceput tema9

---
+++ sprint9-objects/ex2.js
@@ -0,0 +1,8 @@
+//Scrieți o funcție care returnează un obiect nou fără proprietățile furnizate
+//Funcția primește 2 parametri ca date de intrare. Unul dintre parametrii obiectului, 
+// iar celălalt este proprietatea care nu se dorește să persiste în rezultatul returnat.
+
+//Exemplul 1:
+//Intrare: obiect = { a: 1, b: 2 }, proprietate = “b”
+//Ieșire: { a: 1 }
+
---
+++ sprint9-objects/ex3.js
@@ -0,0 +1,10 @@
+//Creați o șir de obiect în care fiecare obiect descrie o carte și are proprietăți precum titlu (un șir de caractere - string), 
+// autor (un șir de caractere - string) și esteCitită (un boolean care indică dacă ați citit-o).
+//Scrieți sau funcționați cu:
+//Iterează prin gama de cărți. Pentru fiecare carte se afișează titlul și autorul cărții astfel: 
+// „Hobbitul de JRR Tolkien”. Utilizează o instrucțiune if / else ce modifică rezultatul, în funcție de fapt că ați citit-o sau nu.
+// Dacă cartea a fost citită, se afișează cu ajutorul funcției console.log un mesaj de tipul „Ai citit deja „Hobbitul” de JRR Tolkien”, dacă nu, se afișează un mesaj de tipul „Trebuie să citești „Hobbitul” de JRR Tolkien”. Exemplul 1:
+//Input: [ { titlu: "Carte 1", autor: "Autor 1", esteCitita: true }, { titlu: "Carte 2", autor: "Autor 2", esteCita: false }, ];
+//Ieșire:
+//“Carte 1 de Autor 1” ”Ai citit deja “Carte 1” de Autor 1” ”Carte 2 de Autor 2” ”Trebuie sa citesti “Carte 2” de Autor 2”
+
