f7caa51b5772b167155314375c3a26f887265e77
denisa lupu <denisalupu2006@gmail.com>
2025-10-08 12:04:01 +0300

    Am inceput tema

---
+++ sprint10-functii/ex4.js
@@ -0,0 +1,13 @@
+//Scrieți o funcție pentru a adăuga numere nelimitate
+//Funcția addNumber() returnează suma tuturor numerelor transmise ca argumente funcției. Numărul de argumente nu este definit. Se poate transmite un număr nelimitat de numere.
+
+//Exemplu 1:
+//Input: addNumber(1, 2, 3)
+//Output: 6
+
+//Exemplu 2:
+//Input: addNumber(1, 2, 3, 4, 5)
+//Output: 15
+
+//Hint: Pentru a rezolva problema, vom folosi operatorul spread / rest (...) pentru a defini argumentele funcției:
+//const addNumber = (…argumente) ⇒ {}
